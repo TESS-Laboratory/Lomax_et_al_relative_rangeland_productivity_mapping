a1b8b0af3bf5d25bfdd3db974ec46189304b1def
G-Lomax <G.Lomax@exeter.ac.uk>
2025-01-10 13:13:00 +0000

    Converted random forest, RESTREND and LGS scripts from RStudio Notebooks to R scripts for improved stability and ease of running on servers.
    Removed additional unused code.

---
+++ analysis/2_quantile_regression_rf.R
@@ -0,0 +1,381 @@
+#' ---
+#' title: "Quantile Regression of Rangeland Productivity - Quantile Regression Forests"
+#' output: html_notebook
+#' author: Guy Lomax
+#' date: 2023-06-19
+#' editor_options: 
+#'   markdown: 
+#'     wrap: 72
+#'   chunk_output_type: console
+#' ---
+#' 
+#' This script conducts quantile regression on rangeland productivity
+#' data using Quantile Regression Forests (QRF), modelling annual gross
+#' primary productivity (GPP) as a function of environmental variables
+#' 
+## ----setup, include = FALSE-------------------------------------------------------------------------------------------------------------------------------------------
+
+# Data handling
+library(tidyverse)
+library(sf)
+library(here)
+
+# Analysis
+library(mlr3verse)
+library(mlr3spatiotempcv)
+library(ranger)
+library(future)
+library(tictoc)
+
+# Visualisation
+library(tmap)
+
+# # Parallelisation
+nc <- 16
+# plan(multicore, workers = nc)
+
+
+#' 
+## ----load--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Country boundaries
+ke_tz <- st_read(here("data", "raw", "vector", "kenya_tanzania.geojson"))
+
+# Sample point data
+sample_points_raw <- st_read(here("data", "raw", "vector", "rangelandSample.geojson"))
+
+twi <- read_csv(here("data", "processed", "csv", "twi_sample.csv"))
+
+
+#' 
+#' Prepare data and add derived variables:
+#' 
+## ----clean-------------------------------------------------------------------------------------------------------------------------------------------
+
+message("Preparing data\n")
+
+yearly_vars <- c("GPP", "precipitation", "pptIntensity", "pptMeanDay", "ugi",
+                 "tMean", "parMean", "potentialET")
+
+sample_points <- sample_points_raw %>%
+  mutate(x = st_coordinates(geometry)[,1],
+         y = st_coordinates(geometry)[,2]) %>%
+  bind_cols(twi) %>%
+  rename(twi = hydrosheds_twi_fd8) %>%
+  select(-allRangelands, -igbp, -shrubland, -grassland, -trees, -ID, -id) %>%
+  pivot_longer(cols = starts_with(yearly_vars),
+               names_to = "var", values_to = "value") %>%
+  separate_wider_delim(var, "_", names = c("data", "year")) %>%
+  pivot_wider(names_from = data, values_from = value) %>%
+  mutate(year = as.numeric(year)) %>%
+  filter(GPP > 0)
+
+# Calculate derived and normalised variables
+# Convert landform to factor variable
+sample_points_derived <- sample_points %>%
+  group_by(index) %>%
+  mutate(mean_ppt = mean(precipitation),
+         ppt_anomaly = (precipitation - mean_ppt) / mean_ppt * 100,
+         mean_pptMeanDay = mean(pptMeanDay),
+         pptMeanDay_anomaly = pptMeanDay - mean_pptMeanDay,
+         landform = factor(landform)) %>%
+  ungroup()
+
+# Save sample points data frame with all variables
+write_csv(sample_points_derived, here("data", "processed", "csv", "rangelandSample_processed.csv"))
+
+#' 
+#' # Data preparation for RF modelling
+#' 
+#' We select variables to use for modelling, remove rows containing NA
+#' values (the TWI raster has some pixels with NA values) and convert to a
+#' spatiotemporal regression task. We assign point id ("id") as our index
+#' of spatial location and "year" as our index of time.
+#' 
+#' We then choose our learner (random forest regression through the ranger
+#' package) with quantile regression enabled, and define two alternative
+#' resampling (cross- validation) strategies: random CV and
+#' "Leave-location-and-time-out" CV that ensures the validation set for
+#' each fold does not overlap in either time or space with the training
+#' set.
+#' 
+#' When defining the learner, we initially choose crude, plausible
+#' hyperparameters of mtry.ratio = 0.5 and sample.fraction = 0.5. However,
+#' we set the initial minimum node size to 100 (around 0.05% of the dataset)
+#' to mitigate overfitting and to allow sufficient terminal node size that
+#' quantiles can be calculated. For our purposes, absolute predictive
+#' accuracy is less important than retaining enough variability in nodes to
+#' allow assessment of quantiles.
+#' 
+## ----data_prep--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Create task
+
+vars_to_retain <- c(
+  "index",
+  "x", "y",
+  "year",
+  "GPP",
+  # "precipitation",  # Exclude raw precipitation data
+  "mean_ppt",
+  "ppt_anomaly",
+  "pptMeanDay_anomaly",
+  "pptIntensity",
+  "ugi",
+  "slope",
+  "distToRiver",
+  "landform",
+  "twi",
+  "tMean",
+  "parMean",
+  "potentialET",
+  "sand",
+  "wriTCFrac"
+)
+
+sample_points_subset <- sample_points_derived %>%
+  drop_na() %>%
+  select(all_of(vars_to_retain))
+
+task_gpp <- as_task_regr_st(
+  x = sample_points_subset,
+  id = "potential_gpp",
+  target = "GPP",
+  coordinate_names = c("x", "y"),
+  coords_as_features = FALSE,
+  crs = "epsg:4326"
+)
+
+task_gpp$set_col_roles("index", roles = "space")
+task_gpp$set_col_roles("year", roles = "time")
+
+# Create learner (ranger random forest) with initial tuning values
+
+lrn_ranger_untuned <- lrn("regr.ranger", 
+                          predict_type = "response",
+                          num.trees = 1001,
+                          mtry.ratio = 0.5,
+                          min.node.size = 100,
+                          sample.fraction = 0.5
+)
+
+# Create resampling strategy
+
+spt_cv_plan <- rsmp("sptcv_cstf", folds = 10)
+# nspt_cv_plan <- rsmp("cv", folds = 10)  # Non-spatiotemporal CV (not used in main analysis)
+
+
+#' 
+#' # Feature selection
+#' 
+#' We use forward feature selection with the untuned model to identify
+#' which covariates have unique information to predict GPP. For this, we
+#' start by building all possible single-variable models. Then, selecting
+#' the variable that gives the best performance (lowest RMSE in this case),
+#' we build all possible two-variable models that include this variable. We
+#' do the same with a third variable, and so on, until the point at which
+#' adding an additional variable fails to improve model performance.
+#' 
+#' We do this separately for models using random CV and spatio-temporal CV,
+#' and save the optimum feature set for each.
+#' 
+## ----rf_feature_select, message = FALSE, results = FALSE, include = FALSE---------------------------------------------------------------------------------------------
+
+message("Beginning feature selection\n")
+
+# Create forward feature selection with untuned model, minimising rmse
+
+perf_msr <- msr("regr.mae")
+
+fs_method <- fs("sequential", min_features = 1)
+
+fs_term <- trm("stagnation_batch")
+
+spt_feature_select <- fsi(
+  task = task_gpp,
+  learner = lrn_ranger_untuned,
+  resampling = spt_cv_plan,
+  measures = perf_msr,
+  terminator = fs_term
+)
+
+# Identify optimal feature set for each resampling strategy and store
+# Time ~ 10-12 hours
+
+# set.seed(123)
+# 
+# tic()
+# progressr::with_progress(
+#   spt_feature_set <- fs_method$optimize(spt_feature_select)
+# )
+# toc()
+# # beep(3)
+# 
+# write_rds(spt_feature_select, here("results", "rds", "spt_feature_selector.rds"))
+# write_rds(spt_feature_set, here("results", "rds", "spt_features.rds"))
+# rm(spt_feature_select, spt_feature_set)
+# gc()
+# pushoverr::pushover("Feature selection complete")
+
+
+#' 
+#' # Hyper-parameter optimisation (model tuning)
+#' 
+#' Now we have identified relevant features (and, hopefully, excluded those
+#' with minimal predictive power), we can use that feature set to tune
+#' hyperparameters for a new set of models.
+#' 
+#' We create two new tasks, each with the variables selected from the
+#' previous step. Then we define a new learner allowing for tuning of
+#' hyperparameters. Finally, we pass the learner and task to an auto-tuner
+#' object for each of the two CV methods. We use a random search to
+#' identify hyperparameters that give decent performance.
+#' 
+## ----rf_tuning_prep, include = FALSE----------------------------------------------------------------------------------------------------------------------------------
+
+message("Beginning hyperparameter tuning\n")
+
+# Load feature set and compare best performance
+
+spt_feature_set <- read_rds(here("results", "rds", "spt_features.rds"))
+
+spt_feature_select <- read_rds(here("results", "rds", "spt_feature_selector.rds"))
+
+spt_feature_set
+
+# Create new task using features selected above
+task_gpp_spt <- task_gpp$clone()
+task_gpp_spt$select(unlist(spt_feature_set$features))
+
+# Define new learner for tuning
+
+lrn_ranger_tuned_spt <- lrn(
+  "regr.ranger", 
+  predict_type = "response",
+  quantreg = TRUE,
+  keep.inbag = TRUE,
+  importance = "permutation",
+  num.trees = 1001,
+  mtry.ratio = to_tune(p_dbl(0, 1)),
+  min.node.size = to_tune(p_int(100, 10000, logscale = TRUE)),
+  sample.fraction = to_tune(p_dbl(0.1, 0.9))
+)
+
+set_threads(lrn_ranger_tuned_spt, 2)
+
+# Define auto-tuner objects for each model
+
+random_tuner <- tnr("random_search")
+
+perf_msr <- msr("regr.rmse")
+
+at_spt <- auto_tuner(
+  tuner = random_tuner,
+  learner = lrn_ranger_tuned_spt,
+  resampling = spt_cv_plan,
+  measure = perf_msr,
+  term_evals = 25
+)
+
+#' 
+#' 
+#' 
+## ----rf_tuning, include=FALSE-----------------------------------------------------------------------------------------------------------------------------------------
+# Spatial model
+
+set.seed(789)
+
+tic()
+rf_tuned_spt <- at_spt$train(task_gpp_spt)
+toc()
+
+write_rds(rf_tuned_spt, here("results", "rds", "rf_tuned_spt.rds"))
+
+#' 
+#' Performance assessment of spatial and non-spatial models, including code for
+#' Predicted vs. Observed GPP (Figure 2).
+#' 
+## ----performance------------------------------------------------------------------------------------------------------------------------------------------------------
+
+rf_tuned_spt <- read_rds(here("results", "rds", "rf_tuned_spt.rds"))
+
+# Measures to evaluate model performance
+measures <- msrs(c("regr.mae", "regr.rmse", "regr.rsq"))
+
+rf_predictions_spt <- rf_tuned_spt$predict(task_gpp_spt)
+
+rf_predictions_spt$score(measures)
+
+# Predicted quantile values
+rf_predictions_spt_qu <- predict(rf_tuned_spt$learner$model, task_gpp_spt$data(), type = "quantiles", quantiles = 0.9)
+
+data_with_quantiles_spt <- task_gpp_spt$data() %>%
+  bind_cols(rf_predictions_spt_qu$predictions) %>%
+  rename(quantile_pred = "quantile= 0.9") %>%
+  mutate(quantile_pred = quantile_pred / 1000,
+         GPP = GPP / 1000)
+
+#' Performance plot (Figure 2(b))
+density_breaks <- c(1, 10, 100, 1000, 10000)
+
+density_plot <- ggplot(data_with_quantiles_spt, aes(x = quantile_pred, y = GPP)) +
+  geom_hex(bins = 60) +
+  scale_fill_viridis_c(direction = 1, trans = "log", breaks = density_breaks) +
+  geom_abline(slope = seq(0.2, 1, 0.2), intercept = 0, colour = "grey", lwd = 0.8, linetype = "longdash") +
+  geom_abline(slope = 1, intercept = 0, colour = "grey", lwd = 1.6) +
+  theme_classic() +
+  theme(legend.position = c(0.18, 0.75), legend.key.height = unit(1.5, "cm"),
+        axis.title = element_text(size = 24),
+        axis.text = element_text(size = 16),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 16),
+        legend.background = element_rect(fill = "transparent")) +
+  xlim(-0.01, 15) +
+  ylim(-0.01, 15) +
+  labs(x = expression(atop("Potential GPP", (kg~C~m^-2~yr^-1))),
+       y = expression(atop("Actual GPP", (kg~C~m^-2~yr^-1))),
+       fill = "Number of points")
+
+ggsave(
+  here("results", "figures", "rf_density_plot.png"),
+  density_plot,
+  width = 24, height = 24, units = "cm", dpi = 250
+)
+
+
+#' 
+#' 
+#' Variable importance plot (Figure 2(a)):
+#' 
+## ----importance-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Variable importance
+
+importance_spt <- rf_tuned_spt$learner$importance()
+
+static_vars <- c("mean_ppt", "slope", "distToRiver", "landform", "twi", "sand", "wriTCFrac")
+
+importance_spt_df <- tibble(variable = names(importance_spt),
+                            importance = as.vector(importance_spt)) %>%
+  mutate(type = ifelse(variable %in% static_vars, "Static", "Dynamic"))
+
+importance_plot <- ggplot(
+  importance_spt_df,
+  aes(x = importance, y = reorder(variable, importance), colour = type)) +
+  geom_point(size = 6) +
+  theme_bw() +
+  scale_colour_manual(values = c("darkblue", "red")) +
+  labs(x = "Variable importance", y = "Variable", colour = "Variable type") +
+  theme(axis.title = element_text(size = 20),
+        axis.text = element_text(size = 16),
+        legend.position = "bottom",
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 16))
+
+ggsave(here("results", "figures", "importance_plot.png"),
+       importance_plot,
+       width = 20, height = 20, units = "cm", dpi = 250)
+  
+
+#' 
+#' 
---
+++ analysis/5_restrend.R
@@ -0,0 +1,233 @@
+#' ---
+#' title: "RESTREND and RUE"
+#' output: html_notebook
+#' date: 2023-11-27
+#' author: "Guy Lomax"
+#' ---
+#' 
+#' This notebook calculates Rain Use Efficiency (RUE) (Le Houerou 1984) and
+#' Residual Trend analysis (RESTREND) (Evans & Geerken 2004; Burrell et al. 2019)
+#' residuals for all grid cells in the study area, as well as temporal trends in
+#' these values, and saves them to disk.
+#' 
+## ----setup------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Data handling
+library(tidyverse)
+library(sf)
+library(terra)
+library(data.table)
+library(here)
+
+# Analysis
+library(tictoc)
+library(future)
+library(furrr)
+
+# Visualisation
+library(tmap)
+
+
+
+#' 
+#' 
+#' Read in data layers
+#' 
+## ----load, include = FALSE--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Country boundaries
+ke_tz <- st_read(here("data", "raw", "vector", "kenya_tanzania.geojson"))
+
+# Covariate layers
+
+# Load dynamic covariates as a named list of rasters (one per year)
+dynamic_covariates_paths <- Sys.glob(here("data", "raw", "raster", "covariateMaps",
+                                          "dynamicVars*.tif"))
+
+years <- str_extract(dynamic_covariates_paths, "\\d\\d\\d\\d")
+dynamic_covariates <- map(dynamic_covariates_paths, rast)
+names(dynamic_covariates) <- years
+
+
+
+#' 
+#' 
+#' Data preparation
+#' 
+## ----data_prep--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Simplify dynamic covariates raster to only GPP and precipitation
+precipitation <- map(dynamic_covariates, function(r) r$precipitation) %>%
+  rast()
+tMean <- map(dynamic_covariates, function(r) r$tMean) %>%
+  rast()
+gpp <- map(dynamic_covariates, function(r) r$GPP) %>%
+  rast()
+
+names(precipitation) <- paste0("precipitation_", years)
+names(tMean) <- paste0("tMean_", years)
+names(gpp) <- paste0("gpp_", years)
+
+
+#' 
+#' 
+#' 
+#' Calculate RUE values and trend for all pixels
+#' 
+## ----rue--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# # Calculate simple rain use efficiency
+# 
+# rue <- gpp / precipitation
+# 
+# writeRaster(rue, here("data", "processed", "raster", "rue", "rue_all.tif"),
+#             overwrite = TRUE)
+# 
+# # Mean RUE
+# rue_mean = mean(rue)
+# names(rue_mean) <- "mean_rue"
+# 
+# # RUE Trend using Theil Sen regression slope
+# tic()
+# rue_trend <- app(rue, function(ts) {
+#   if (any(is.na(ts))) {
+#     rep(NA, 3)
+#   } else {
+#     year <- 1:19
+#     df <- data.frame(year = year, ts = ts)
+#     
+#     mod <- RobustLinearReg::theil_sen_regression(ts ~ year, data = df)
+#     
+#     slope <- coef(mod)[2]
+#     r_sq <- broom::glance(mod)$r.squared[1]
+#     p_value <- broom::tidy(mod)$p.value[2]
+#     
+#     c(slope, r_sq, p_value)
+#   }
+# })
+# toc()
+# 
+# names(rue_trend) <- c("rue_slope", "r_sq", "p_value")
+# 
+# # Write rasters to disk
+# writeRaster(rue_mean, here("data", "processed", "raster", "rue", "rue_mean.tif"), overwrite = TRUE)
+# writeRaster(rue_trend, here("data", "processed", "raster", "rue", "rue_trend.tif"), overwrite = TRUE)
+
+
+#' 
+#' Calculate RESTREND residuals and residual trend for all pixels
+#' 
+## ----restrend---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Custom function to extract model params and residuals from linear models
+# fitted to the time series in each pixel
+
+fit_restrend <- function(gpp_rast, ppt_rast, tMean_rast) {
+  
+  # Convert rasters to a single data frame
+  message("Converting to data frames\n")
+
+  gpp_values <- as.data.frame(gpp_rast, cells = TRUE, xy = TRUE, na.rm = T)
+  ppt_values <- as.data.frame(ppt_rast, cells = TRUE, xy = FALSE, na.rm = T)
+  tMean_values <- as.data.frame(tMean_rast, cells = TRUE, xy = FALSE, na.rm = T)
+  
+  data_df <- gpp_values %>%
+    left_join(ppt_values, by = "cell") %>%
+    left_join(tMean_values, by = "cell") %>%
+    pivot_longer(cols = starts_with(c("gpp", "precipitation", "tMean"))) %>%
+    separate_wider_delim(cols = "name", delim = "_", names = c("var", "year")) %>%
+    pivot_wider(id_cols = c(cell, x, y, year), names_from = var, values_from = value) %>%
+    drop_na()
+  
+  # Apply RESTREND function to each cell in data frame
+  message("Building models and calculating residuals\n")
+  
+  apply_restrend <- function(df) {
+    
+    model <- lm(gpp ~ precipitation + tMean, data = df)
+
+    yint <- coef(model)[1]
+    ppt_slope <- coef(model)[2]
+    t_slope <- coef(model)[3]
+
+    r_sq <- broom::glance(model)$r.squared
+    ppt_p_value <- broom::tidy(model)$p.value[2]
+    t_p_value <- broom::tidy(model)$p.value[3]
+
+    resids <- residuals(model)
+    
+    if (length(resids) == nlyr(gpp_rast)) {
+      
+      output <- c(yint, ppt_slope, t_slope, r_sq, ppt_p_value, t_p_value, resids)
+      
+    } else {
+      
+      output <- rep(NA, nlyr(gpp_rast) + 6)
+      
+    }
+    
+    names(output) <- c("yint", "ppt_slope", "t_slope",
+                         "rsq", "ppt_p_value", "t_p_value", 
+                         paste0("resid_", years))
+    
+    output
+  }
+  
+  restrend_df <- data_df %>%
+    group_by(cell, x, y) %>%
+    nest() %>%
+    ungroup() %>%
+    mutate(restrend_list = future_map(data, apply_restrend)) %>%
+    select(x,y,restrend_list) %>%
+    unnest_wider(restrend_list)
+  
+  # Convert to raster
+  message("Converting back to raster")
+  restrend_rast <- rast(restrend_df, crs = crs(gpp_rast), extent = ext(gpp_rast))
+}
+
+tictoc::tic()
+restrend_rast <- fit_restrend(gpp, precipitation, tMean)
+tictoc::toc()
+writeRaster(restrend_rast, here("data", "processed", "raster", "restrend", "restrend_resids.tif"),
+            overwrite = TRUE)
+
+pushoverr::pushover("RESTREND residuals calculated")
+
+# Calculate residual trend and save
+
+restrend_rast <- rast(here("data", "processed", "raster", "restrend", "restrend_resids.tif"))
+
+calculate_trend <- function(x) {
+  
+  if (any(is.na(x))) {
+    
+    rep(NA, 3)
+  
+  } else {
+    
+    year <- 1:19
+    mod <- RobustLinearReg::theil_sen_regression(x ~ year)
+    
+    resid_slope <- coef(mod)[2]
+    resid_r_sq <- broom::glance(mod)$r.squared[1]
+    resid_p_value <- broom::tidy(mod)$p.value[2]
+    
+    c(resid_slope, resid_r_sq, resid_p_value)
+  }
+  
+}
+
+tic()
+restrend_results <- app(restrend_rast[[7:25]], calculate_trend)
+toc()
+
+names(restrend_results) <- c("resid_slope", "resid_rsq", "resid_p_value")
+
+writeRaster(restrend_results, here("data", "processed", "raster", "restrend_results.tif"),
+            overwrite = TRUE)
+
+
+pushoverr::pushover("RESTREND analysis complete")
+
+#' 
